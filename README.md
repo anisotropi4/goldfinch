64ab169c543b97b4b326e2c2530b655a4004ad07
Guido Eco <guido.eco@crinstitute.org.uk>
2017-04-09 16:01:42 +0100

    Commit postgres create_table.py with comments

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # goldfinch
 A set of scripts for working with postgres and arangodb databases based on extending Jeroen Janssens 'Data Science on the Command Line' https://github.com/jeroenjanssens/data-science-at-the-command-line  
+
+1) create_table.py: Based on column names in a tsv file-format this python3 script create a postgres import script for the file called table_<filename-in-lower-cast>. 
+
+To import the file 'CORPUS.tsv' with user 'finch' on the postgres server 'raven' run:
+
+`$ < table_CORPUS.sql psql -U finch -h raven` 
+
+The table created will be called table_corpus. 
+All columns are varchar by default but can be modified ahead of import.
+Edit the import script as indentified in the script to supports csv rather than tsv import. 
---
+++ bin/create_table.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+import csv
+import sys
+import os
+import re
+
+filename = sys.argv[1]
+
+tablename = filename
+
+#[1/3] For csv file uncomment the next line and comment out tsv
+#tablename = re.sub(r'\.csv$', '', tablename)
+tablename = re.sub(r'\.tsv$', '', tablename)
+
+tablename = re.sub(r'[ \.()\/\-]', '_', tablename)
+tablename = re.sub(r'^', 'table_', tablename)
+
+instances = {}
+attributes = []
+unique = True
+
+s1_re = re.compile('[ ()\/\-\\%]+')
+s2_re = re.compile('[\.:\?%]')
+s3_re = re.compile('#')
+s4_re = re.compile('£')
+s5_re = re.compile('^_+')
+s6_re = re.compile('_+$')
+
+print(tablename)
+with open(filename) as csvfile:
+    #[2/3] For csv file uncomment the next line and comment out tsv
+    #reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    reader = csv.reader(csvfile, delimiter='\t', quotechar='"')
+    for fields in reader:
+        count = 0
+        for field in fields:
+            field = s1_re.sub('_',field.lower())
+            field = s2_re.sub('',field)
+            field = s3_re.sub('n',field)
+            field = s4_re.sub('GBP',field)
+            field = s5_re.sub('',field)
+            field = s6_re.sub('',field)
+            if field == "":
+                field = "empty_"+str(len(attributes))
+
+            if field in instances:
+                unique = False
+                instances[field] += 1
+            else:
+                instances[field] = 1
+            attributes.append(field)
+        break
+
+
+if unique:
+    with open(tablename+'.sql','w+') as sqlfile:
+        sqlfile.write('drop table {} cascade;\n'.format(tablename))
+        sqlfile.write('create table {} ('.format(tablename))
+        sqlfile.write(' varchar, '.join(attributes)+' varchar);\n')
+        #[3/3] For csv file uncomment the next line and comment out tsv
+        #sqlfile.write("\\copy {0} from '{1}/{2}' csv header\n".format(tablename,os.getcwd(),filename))
+        sqlfile.write("\\copy {0} from '{1}/{2}' delimiter as '\t' csv header\n".format(tablename,os.getcwd(),filename))
+else:
+    print('**duplicate fields')
+
+for field in attributes:
+    print('{0},{1}'.format(field,instances[field]))
+
+print('')
