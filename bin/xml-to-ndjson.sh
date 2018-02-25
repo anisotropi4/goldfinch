74639ec3c61c3a8a5eb1570e85f9918b2c5d1081
Will Deakin <will.deakin@crinstitute.org.uk>
2018-02-25 20:59:48 +0000

    Update with .xml filename filter

--- bin/xml-to-ndjson.sh
+++ bin/xml-to-ndjson.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-
 FILENAME=$1
+FILENAME=$(echo ${FILENAME} | sed 's/.xml$//')
 TAG=$2
+TAG=${TAG:-${FILENAME}}
 XTAG=$3
 XTAG=${XTAG:-"_wrapper"}
 
-xml-to-json ${FILENAME} | jq -c ".${XTAG}.${TAG}[]"
+xml-to-json ${FILENAME}.xml | jq -c ".${XTAG}.${TAG}[]"
 
