58e8e4c397acf2cd9c57a893ed9977de3e72ada6
Will Deakin <will.deakin@crinstitute.org.uk>
2018-04-01 22:59:14 +0100

    simplify query, fix sort order and add timer

--- overlapfilter/filter02.py
+++ overlapfilter/filter02.py
@@ -3,6 +3,8 @@ import sys
 from pyArango.connection import *
 import os
 import requests
+from operator import itemgetter
+from datetime import datetime
 
 ARUSR = os.environ['ARUSR']
 ARPWD = os.environ['ARPWD']
@@ -19,37 +21,39 @@ let nodes = (for j in within(fullfilternodes, i.lat, i.lon, @n) \
 filter i._key != j._key \
 filter j.valid > @n \
 return j._key) \
-let n = length(nodes) \
-filter n > 0 \
-sort n desc \
 return {node: i._key, nodes: nodes, valid: i.valid}'
 
 update = 'for i in @a update i.k with { "valid": i.v } in fullfilternodes'
 
+print('\t'.join(['n','count', 'elapsed']))
 n = 2
-
-print("n	count")
-
 for k in range(22):
+    s0 = datetime.now()
     results = db.AQLQuery(query, rawResults=True, bindVars={'n': n})
 
     node = {}
+    sequence = []
 
     for i in results:
-        node[i["node"]] = {"nodes": i["nodes"], "valid": i["valid"] }
+        m = len(i['nodes'])
+        if m > 0:
+            node[i["node"]] = {"nodes": i["nodes"], "valid": i["valid"] }
+            sequence.append((i['node'], m))
 
     documents = {}
-    for i in node.keys():
+    for (i, _) in sorted(sequence, key=itemgetter(1), reverse=True):
         if (node[i]["valid"] > n):
             for j in node[i]["nodes"]:
                 if j in node:
                     documents[j] = n // 2
 
-    count = len(documents)
-    print(n // 2, '	', count)
-    if(count <= 1):
-        sys.exit()
-
     r = db.AQLQuery(update, bindVars={'a': [{'k': k, 'v': v} for k, v in documents.items()]})
+
+    count = len(documents)
+    s1 = datetime.now()
+
+    print('\t'.join([str(i) for i in [n // 2, count, (s1 - s0).total_seconds()]]))
+
+    if(count <= 1): break
 
     n = n * 2
