0d1e62d824b21ff39b458ea83fab1678f5204929
Will Deakin <will.deakin@crinstitute.org.uk>
2018-12-23 11:26:22 +0000

    update to add osm generated points

--- timetable/run.sh
+++ timetable/run.sh
@@ -6,25 +6,21 @@ if [ ! -f tbody.html ]; then
     curl ${URL} | scrape -e 'tbody' > tbody.html
 fi
 
-if [ ! -f file-list.txt ]; then
+if [ ! -f full-file-list.txt ]; then
     xml-to-json tbody.html | jq -cr '.tbody[] | .[].td[] | select(.a?.title?) | .a.title' > full-file-list.txt
 fi
 
-if [ -f full-file-list.txt ]; then
-    LINE=$(fgrep -n _full.gz full-file-list.txt | tail -1)
-    DATESTRING=$(echo ${LINE} | cut -d'_' -f1 | cut -d':' -f2 | cut -c1-8)
-    if [ ! -f file-list.txt ]; then
-         N=$(echo ${LINE} | cut -d':' -f1)
-         tail -n +${N} full-file-list.txt > file-list.txt
-    fi    
-fi
+LINE=$(fgrep -n _full.gz full-file-list.txt | tail -1)
 
-echo ${DATESTRING}
+if [ ! -f file-list.txt ]; then
+    N=$(echo ${LINE} | cut -d':' -f1)
+    tail -n +${N} full-file-list.txt > file-list.txt
+fi    
 
 for FILENAME in $(cat file-list.txt | sed 's/.gz$//')
 do
     if [ ! -f data/${FILENAME} ]; then
-        curl -o data/${FILENAME} ${URL}/${FILENAME}
+        curl -o data/${FILENAME}.gz ${URL}/${FILENAME}.gz
         gzip -d data/${FILENAME}.gz
     fi
 
@@ -38,12 +34,14 @@ do
     fi
 done
 
+DATESTRING=$(tail -1 file-list.txt | cut -d'_' -f1 | cut -d':' -f2 | cut -c1-8)
+echo ${DATESTRING}
+
 if [ ! -f timetable-${DATESTRING}.ndjson ]; then
     cat schedule/*-path | ./wtt-timetable2.py > timetable-${DATESTRING}.ndjson
 fi
 
-
-FILTER=20180925/20180926
+FILTER=20181214/20181215
 if [ ! -f wtt-${DATESTRING}.ndjson ]; then
     < timetable-${DATESTRING}.ndjson ./wtt-select2.py ${FILTER} > wtt-${DATESTRING}.ndjson
 fi
--- timetable/wtt-map.py
+++ timetable/wtt-map.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import pandas as pd
+from pandas.io.json import json_normalize
 import json
 import numpy as np
 import sys
@@ -8,40 +9,78 @@ from datetime import datetime, date, timedelta, time, MINYEAR
 from iso8601datetime.interval import interval_p
 from iso8601datetime.duration import duration_f, duration_p, time_f, fromisoday_p
 
-df1 = pd.read_json('TIPLOC_Eastings_and_Northings.json')
-
 locations = {}
-for index, row in df1.iterrows():
-    locations[row['TIPLOC']] = {'station': row['NAME'], 'longitude': row['longitude'], 'latitude': row['latitude']}    
-
-
-df1 = pd.read_csv('NaPTAN-Rail2.tsv', sep='\t')
-naptan = {}
-
-for index, row in df1.iterrows():
-    if row['TiplocRef'] != '':
-        naptan[row['TiplocRef']] = {'station': row['StationName'], 'longitude': row['Longitude'], 'latitude': row['Latitude']}
 
 def trim_f(this_object):
-    if this_object:
-        return float('{0:.4f}'.format(this_object))
-    return this_object
+    v = this_object
+    if not this_object:
+        return v
+    if type(this_object) is str:
+        v = float(this_object)
+    return float('{0:.4f}'.format(v))
+
+## Decode GeoJson object:
+# {"type": "FeatureCollection",
+#  "name": "TIPLOC",
+#  "crs": {
+#    "type": "name",
+#    "properties": {
+#      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+#    }
+#  },
+#  "features": [...]}
+
+with open('TIPLOC_Eastings_and_Northings.json', 'rb') as fp:
+    geoobject_json = json.load(fp)
+    for this_object in geoobject_json['features']:
+        properties = this_object['properties']
+        (longitude, latitude) = this_object['geometry']['coordinates']
+        locations[properties['TIPLOC']] = {'station': properties['NAME'], 'longitude': trim_f(longitude), 'latitude': trim_f(latitude)}
+
+df1 = pd.read_csv('TIPLOC-map.tsv', sep='\t')
+tiploc_map = df1.set_index('NaPTAN').to_dict('index')
+
+naptan = {}
+with open('NaPTAN-Rail.ndjson', 'rb') as fp:
+    for line in fp:
+        this_object = json.loads(line)
+        (longitude, latitude) = (this_object['lat'], this_object['lon'])
+        tiploc = this_object['TIPLOC']
+        naptan[tiploc] = {'name': this_object['Name'], 'longitude': trim_f(longitude), 'latitude': trim_f(latitude)}
+        if tiploc in tiploc_map:
+            naptan[tiploc_map[tiploc]['TIPLOC']] = {'name': this_object['Name'], 'longitude': trim_f(longitude), 'latitude': trim_f(latitude), 'lookup': True}
+
+osmdata = {}
+with open('osmrail-all.ndjson', 'rb') as fp:
+    for line in fp:
+        this_object = json.loads(line)
+        (longitude, latitude) = (this_object['lat'], this_object['lon'])
+        if 'name' not in this_object:
+            sys.stderr.write('No name: {}\n'.format(json.dumps(this_object)))
+            continue
+        osmdata[this_object['TIPLOC']] = {'name': this_object['name'], 'longitude': trim_f(longitude), 'latitude': trim_f(latitude)}
+
+#df1 = pd.read_csv('NaPTAN-Rail2.tsv', sep='\t')
+#for index, row in df1.iterrows():
+#    if row['TiplocRef'] != '':
+#        naptan[row['TiplocRef']] = {'station': row['StationName'], 'longitude': row['Longitude'], 'latitude': row['Latitude']}
 
 def coordinates(location_str):
     this_object = {}
     if location_str in locations:
         this_object = locations[location_str]
-    if location_str in naptan:
+    elif location_str in naptan:
         this_object = naptan[location_str]
-    return (any(this_object), trim_f(this_object.get('longitude')),
-            trim_f(this_object.get('latitude')))
+    elif location_str in osmdata:
+        this_object = osmdata[location_str]
+    return (any(this_object), this_object.get('longitude'), this_object.get('latitude'))
 
 for line in sys.stdin:
     object_json = json.loads(line)
     UID = object_json['UID']
     headcode = object_json.get('Service').get('BS').get('Headcode')
     schedule = object_json['Schedule']
-    (start_interval, end_interval) = interval_p(object_json['Interval'], return_duration=False)    
+    (start_interval, end_interval) = interval_p(object_json['Interval'], return_duration=False)
     operation_date = object_json['Day']
     start_service = datetime.combine(fromisoday_p(operation_date).date(), start_interval.time())
 
@@ -64,6 +103,3 @@ for line in sys.stdin:
                          'lat': lat}
 
         print(json.dumps(output_object))
-
-
-
