8012f6fb85c24e52395e0db8b2d6c16c6cf371ad
Will Deakin <will.deakin@crinstitute.org.uk>
2019-01-13 19:23:24 +0000

    cannot test for arangodb up

--- ogrrailway/create-ogrdb.sh
+++ ogrrailway/create-ogrdb.sh
@@ -8,11 +8,6 @@ PATH=${PATH}:./bin
 export ARUSR ARDBN
 . bin/ar-env.sh
 
-if [ x$(aqlx.sh 'return 0' 2> /dev/null) != x0 ]; then
-    echo "an arangodb instance does not appear to be running on 'ar-server'"
-    exit 2
-fi
-
 sh bin/createdb.sh
 
 < great-britain-railway.ndjson arangoimp --file - --type json --batch-size 134217728 --progress true --server.username ${ARUSR} --server.authentication true --server.database ${ARDBN} --server.endpoint "tcp://"${ARSVR}":8529" --server.password "${ARPWD}" --collection fullosm2 --create-collection true --overwrite true
