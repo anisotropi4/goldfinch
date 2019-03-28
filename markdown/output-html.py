aea441f25ca98217ca73ebb501ecf8c38cd37f57
Will Deakin <will.deakin@crinstitute.org.uk>
2019-03-28 21:16:02 +0000

    merge branch

--- markdown/output-html.py
+++ markdown/output-html.py
@@ -12,19 +12,20 @@ this_template = """<html>
 <title>{{ titletext }}</title>
 </head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="github-markdown.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css">
 <style>
 	.markdown-body {
 		box-sizing: border-box;
 		min-width: 200px;
 		max-width: 980px;
 		margin: 0 auto;
-		padding: 45px;
 	}
-
+        .markdown-body table td {
+                background-color: white;
+        }
 	@media (max-width: 767px) {
 		.markdown-body {
-			padding: 15px;
+			padding: 0px;
 		}
 	}
 </style>
