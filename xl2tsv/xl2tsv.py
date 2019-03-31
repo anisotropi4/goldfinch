2e5f5a8e11974da9778127d8120082d8019778ed
Will Deakin <will.deakin@crinstitute.org.uk>
2019-04-01 00:13:51 +0100

    update for argparse options

--- xl2tsv/xl2tsv.py
+++ xl2tsv/xl2tsv.py
@@ -9,17 +9,25 @@ parser = argparse.ArgumentParser(description='Dump xls(x) files tab(s) to .tsv f
 
 parser.add_argument('inputfiles', type=str, nargs='*', help='name of xls-file to process')
 
-parser.add_argument('--path', dest='path', type=str, default='output',
-                    help='output directory file')
 
-parser.add_argument('--tab', type=str, dest='tab', default=None,
-                    help='name of tab to process')
+tabgroup = parser.add_mutually_exclusive_group()
 
-parser.add_argument('--tabnames', dest='tabnames', action='store_true',
+tabgroup.add_argument('--tabnames', dest='tabnames', action='store_true',
                     default=False, help='dump name of tabs')
 
-parser.add_argument('--filename', dest='filename', action='store_true',
-                    default=False, help='add filename to output file')
+tabgroup.add_argument('--tab', type=str, dest='tab', default=None,
+                    help='name of tab to process')
+
+filegroup = parser.add_mutually_exclusive_group()
+
+filegroup.add_argument('--path', dest='path', type=str, default='output',
+                    help='output directory file')
+
+filegroup.add_argument('--stdout', dest='stdout', action='store_true',
+                    default=False, help='dump a tab to stdout')
+
+parser.add_argument('--sourcename', dest='sourcename', action='store_true',
+                    default=False, help='prepend filename to output tab file')
 
 args = parser.parse_args()
 
@@ -36,23 +44,26 @@ if args.tabnames:
         print('\t'.join(df.keys()))
     sys.exit(0)
 
-for filename in args.inputfiles:    
+for filename in args.inputfiles:
     if args.tab:
         tab = args.tab
         filebase = ''
-        if args.filename:
+        if args.sourcename:
             filebase = filename + ':'
             if '.' in filename:
                 filebase = filename.rsplit('.', 1)[0] + ':'
         try:
             df = pd.read_excel(filename, tab)
-            df.to_csv('{}/{}{}.tsv'.format(path, filebase, tab), index=False, sep='\t')
+            if args.stdout:
+                df.to_csv(sys.stdout, index=False, sep='\t')
+            else:
+                df.to_csv('{}/{}{}.tsv'.format(path, filebase, tab), index=False, sep='\t')
         except KeyError:
             pass
     else:
         df = pd.read_excel(filename, None)
         filebase = ''
-        if args.filename:
+        if args.sourcename:
             filebase = filename + ':'
             if '.' in filename:
                 filebase = filename.rsplit('.', 1)[0] + ':'
