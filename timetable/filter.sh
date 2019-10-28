85d479798c1ab1656a7300e77e5a46ff145bfcc7
Will Deakin <will.deakin@crinstitute.org.uk>
2019-10-28 08:49:40 +0000

    update for wtt-select4 and 2019 default date

--- timetable/filter.sh
+++ timetable/filter.sh
@@ -3,14 +3,14 @@
 DATESTRING=$(tail -1 file-list.txt | cut -d'_' -f1 | cut -d':' -f2 | cut -c1-8)
 echo ${DATESTRING}
 
-FILTER=20181214/20181215
+FILTER=20191208/20181209
 FILTER=${1:-$FILTER}
 
 while true
 do
     echo ${FILTER}
     if [ ! -f wtt-${DATESTRING}-$#.ndjson ]; then
-        < timetable-${DATESTRING}.ndjson ./wtt-select3.py ${FILTER} > wtt-${DATESTRING}-$#.ndjson
+        < timetable-${DATESTRING}.ndjson ./wtt-select4.py ${FILTER} > wtt-${DATESTRING}-$#.ndjson
     fi
 
     if [ ! -f output-all.json ]; then
