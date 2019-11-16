391861380e93870d42a41b02946cf0ea541e0d7a
Will Deakin <will.deakin@crinstitute.org.uk>
2019-11-16 22:36:31 +0000

    update for tsv not jsonl

--- timetable/filter.sh
+++ timetable/filter.sh
@@ -3,19 +3,19 @@
 DATESTRING=$(tail -1 file-list.txt | cut -d'_' -f1 | cut -d':' -f2 | cut -c1-8)
 echo ${DATESTRING}
 
-FILTER=20191208/20181209
+FILTER=20191208/20191209
 FILTER=${1:-$FILTER}
 
 while true
 do
     echo ${FILTER}
-    if [ ! -f wtt-${DATESTRING}-$#.jsonl ]; then
+    if [ ! -s wtt-${DATESTRING}-$#.jsonl ]; then
         ./wtt-select4.py PA-${DATESTRING}.jsonl ${FILTER} > wtt-${DATESTRING}-$#.jsonl
     fi
 
     if [ ! -s output-all.json ]; then
-        < wtt-${DATESTRING}-$#.jsonl ./wtt-map.py > output-all.jsonl 2> missing-TIPLOC.jsonl
-        < output-all.json sort -n | jq -sc '.' > output-all.json
+        < wtt-${DATESTRING}-$#.jsonl ./wtt-map.py > output-all.jsonl 2> missing-TIPLOC.tsv
+        < output-all.jsonl sort -n | jq -sc '.' > output-all.json
         echo "TIPLOC	HeadCode	count" > missing-report.tsv
         < missing-TIPLOC.tsv cut -f1-2 -d'	' | unip >> missing-report.tsv
     fi
