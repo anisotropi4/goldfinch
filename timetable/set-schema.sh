2ce79342d16950f93453dbb564998d0a39d5a31e
Will Deakin <will.deakin@crinstitute.org.uk>
2019-11-16 22:39:59 +0000

    now uses ID parameter

--- timetable/set-schema.sh
+++ timetable/set-schema.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-SCHEMA=${1:-schema.jsonl}
+KEY=$1
+SCHEMA=${2:-schema.jsonl}
 
 if [ ! -s ${SCHEMA} ]; then
     echo Error set-schema.sh: empty or missing ${SCHEMA} file
 fi
 
-./solr/create-cores.sh $(< ${SCHEMA} jq -r 'keys[0]' | sort -u)
+./solr/create-core.sh ${KEY}
 
-for KEY in $(< ${SCHEMA} jq -r 'keys[0]' | sort -u)
-do
-    < ${SCHEMA} jq -c 'select(.'${KEY}') | .[][]' | \
-        while read -r FIELD
-        do
-            ./solr/update-field.sh ${KEY} "${FIELD}"            
-        done 
-done
+< ${SCHEMA} jq -c 'select(.'${KEY}') | .[][]' | \
+    while read -r FIELD
+    do
+        ./solr/update-field.sh ${KEY} "${FIELD}"
+    done
+
