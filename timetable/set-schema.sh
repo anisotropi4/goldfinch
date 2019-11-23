9a757941ec5c549f382021710efd4542f9c43b93
Will Deakin <will.deakin@crinstitute.org.uk>
2019-11-23 23:15:33 +0000

    create solr datefield type

--- timetable/set-schema.sh
+++ timetable/set-schema.sh
@@ -10,8 +10,10 @@ fi
 ./solr/create-core.sh ${KEY}
 
 < ${SCHEMA} jq -c 'select(.'${KEY}') | .[][]' | \
-    while read -r FIELD
+    while read -r JSON
     do
-        ./solr/update-field.sh ${KEY} "${FIELD}"
+        FIELD=$(echo "${JSON}" | jq 'keys[]')
+        TYPE=$(echo "${JSON}" | jq '. | to_entries[] | .value')
+        echo ${KEY} "${FIELD}" "${TYPE}" 
+        ./solr/update-field.sh ${KEY} "${FIELD}" "${TYPE}"
     done
-
