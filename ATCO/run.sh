4f8ba68fc7ab5e45f8d85f6ee8aecdcf1f933341
Will Deakin <will.deakin@crinstitute.org.uk>
2021-04-05 16:27:35 +0100

    simplify scrape using xpath

--- ATCO/run.sh
+++ ATCO/run.sh
@@ -2,11 +2,6 @@
 
 URL=https://www.gov.uk/government/publications/national-public-transport-access-node-schema/naptan-guide-for-data-managers
 
-curl -s -L -G ${URL} | \
-    ./htmltojson.py --depth 4 --stdout | \
-    jq -c '.div[]? | select(.class == "govuk-grid-row sidebar-with-body")? | .div[] | select(.class == "main-content-container") | .div[] | select(.class)? | .div | .table[]' | \
-    jq -c '[{"th": [.thead.tr.th[].value]}, .tbody.tr[]]' | \
-    sed 's/null/""/g' | \
-    ./scrape.py
+./scrape-tables.py ${URL}
 
 
